619ec13fc9f8a4f6aac9cacb2cbc412bad8bcdf3
Mathias Nohall <mathias.nohall@valtech.se>
2016-07-19 16:28:56 +0200

    added initializationmodule

---
+++ EPiLastic.Indexing/Initialization/EPiLasticInitalizationModule.cs
@@ -0,0 +1,42 @@
+﻿using EpiLastic.Indexing;
+using EpiLastic.Indexing.EventHandling;
+using EpiLastic.Indexing.ReIndexJob;
+using EpiLastic.Indexing.Services;
+using EpiLastic.Services;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+
+namespace EPiLastic.Indexing.Initialization
+{
+    [ModuleDependency(typeof(ServiceContainerInitialization))]
+    [InitializableModule]
+    public class EPiLasticInitalizationModule : IConfigurableModule
+    {
+        void IConfigurableModule.ConfigureContainer(ServiceConfigurationContext context)
+        {         
+            context.Container.Configure(c => c.For<IIndexClient>().Use<IndexClient>());
+            context.Container.Configure(c => c.For<IReIndexJobHandler>().Use<ReIndexJobHandler>());
+            context.Container.Configure(c => c.For<IEPiServerEventHandler>().Use<EPiServerEventHandler>());
+            context.Container.Configure(c => c.For<IPageHelper>().Use<PageHelper>());
+            context.Container.Configure(c => c.For<IReIndexJobHandler>().Use<ReIndexJobHandler>());
+            context.Container.Configure(c => c.For<IIndexingHandler>().Use<IndexingHandler>());
+            context.Container.Configure(c => c.For<EpiLastic.Wrappers.IDateTimeWrapper>().Use<EpiLastic.Wrappers.DateTimeWrapper>());
+            context.Container.Configure(c => c.For<ISuggestionHelper>().Use<SuggestionHelper>());
+
+            context.Container.Configure(c => c.For<ISearchResponseMapper>().Use<SearchResponseMapper>());
+        }
+
+        public void Initialize(InitializationEngine context)
+        {
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+
+        public void Preload(string[] parameters)
+        {
+        }
+    }
+}
---
+++ EPiLastic.Querying/Initialization/EPiLasticInitalizationModule.cs
@@ -0,0 +1,29 @@
+﻿using EpiLastic.Querying;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+
+namespace EPiLastic.Querying.Initialization
+{
+    [ModuleDependency(typeof(ServiceContainerInitialization))]
+    [InitializableModule]
+    public class EPiLasticInitalizationModule : IConfigurableModule
+    {
+        void IConfigurableModule.ConfigureContainer(ServiceConfigurationContext context)
+        {         
+            context.Container.Configure(c => c.For<ISearchClient>().Use<SearchClient>());
+        }
+
+        public void Initialize(InitializationEngine context)
+        {
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+
+        public void Preload(string[] parameters)
+        {
+        }
+    }
+}
